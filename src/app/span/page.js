7234723c9bc22c67cbe72442cca58aeff1a48134
Vikysan <96440010+Vikysan@users.noreply.github.com>
2025-07-28 11:44:53 +0200

    loading page

--- src/app/span/page.js
+++ src/app/span/page.js
@@ -1,11 +1,14 @@
 "use client"
 import React from 'react'
+import { useRouter } from 'next/navigation'
 import { data } from '@/data/100'
 
 const page = () => {
+  const router = useRouter()
   return (
     <form action={(e)=>{
       console.log(e)
+      router.push('/test/20-30')
     }}>
       <input type="number" name='begin' min={1} max={data.length-1} defaultValue={1}/>
       <input type="number" name="end" min={1} max={data.length-1} defaultValue={data.length-1}/>
--- src/app/test/[span]/page.js
+++ src/app/test/[span]/page.js
@@ -15,15 +15,15 @@ import {
 } from "next/navigation";
 import { Fragment, useState, useEffect } from "react";
 import { data } from "@/data/100";
-import { HandRaisedIcon,CheckCircleIcon } from "@heroicons/react/24/solid";
+import { HandRaisedIcon, CheckCircleIcon } from "@heroicons/react/24/solid";
 const options = data[0].options;
 
 const Example = () => {
   const params = useParams();
-  const span = params.span.split("-").map(n=>+n-1)
-  const [selected, setSelected] = useState();
-  const [inputData, setInputData] = useState(data.slice(span[0],span[1]));
+  const span = params.span.split("-").map((n) => +n - 1);
+  const [inputData] = useState(data.slice(span[0], span[1]));
   const [counter, setCounter] = useState(0);
+  const [selected, setSelected] = useState(inputData[counter].options[0]);
   const [answers, setAnswers] = useState([]);
 
   const maxCounter = inputData.length - 1;
@@ -35,12 +35,12 @@ const Example = () => {
         option == selected ? { ...option, select: true } : option
       ),
     ]);
-    counter < maxCounter ? setCounter((prev) => {return prev+1}) : "";
+    counter < maxCounter
+      ? setCounter((prev) => {
+          return prev + 1;
+        })
+      : "";
   };
-
-  
-
-
 
   return (
     <Fieldset>
@@ -54,37 +54,39 @@ const Example = () => {
           <Field
             key={option.text}
             //   disabled={!plan.available}
-            className="flex items-center gap-2"
+            className="flex  items-center gap-2"
           >
-            <Radio as={Fragment} value={option} className="group">
+            <Radio as={Fragment} value={option}>
               {({ checked, disabled }) => (
-                <div
-                  className={`group flex size-5 items-center justify-center rounded-full border
+                <div className="flex  group bg-red-400 data-checked:bg-amber-200 ">
+                  <span
+                  // className="inline w-1"
+                    className={` flex size-5 items-center justify-center rounded-full border
                     ${checked ? "bg-blue-400" : "bg-white"}  ${
-                    disabled && "bg-gray-100"
-                  }`}
-                >
-                  {/* {checked && <span className="size-2 rounded-full bg-white" />} */}
-                   <CheckCircleIcon className="size-6 fill-white opacity-0 transition group-data-checked:opacity-100" />
+                      disabled && "bg-gray-100"
+                    }`}
+                  >
+                    {/* {checked && <span className="size-2 rounded-full bg-white" />} */}
+                    <CheckCircleIcon className="size-6 fill-white opacity-0 transition group-data-checked:opacity-100" />
+                  </span>
+                 
+                 <p className="inline">
+                   {option.text}
+                  </p> 
+                  {/* <Label as={Fragment}>
+                    {({ disabled }) => (
+                      <label className={disabled && "opacity-50"}>
+                        {option.text}
+                      </label>
+                    )}
+                  </Label> */}
                 </div>
-                
               )}
             </Radio>
-            <Label as={Fragment}>
-              {({ disabled }) => (
-                <label className={disabled && "opacity-50"}>
-                  {option.text}
-                </label>
-              )}
-            </Label>
           </Field>
         ))}
       </RadioGroup>
-      <button
-        onClick={addAnswerHandler}
-      >
-        Dalši
-      </button>
+      <button onClick={addAnswerHandler}>Dalši</button>
     </Fieldset>
   );
 };
