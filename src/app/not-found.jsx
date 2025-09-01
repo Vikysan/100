732e7b15eb13c08b732ba640fce8a2b6dba85cb7
Vikysan <vikysamachan@gmail.com>
2025-09-01 11:32:59 +0200

    feat: add not found page component

---
+++ src/app/not-found.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const notFoundPage = () => {
+  return (
+    <div>
+      <h1>something really dirty and brown was committed</h1>
+    </div>
+  )
+}
+
+export default notFoundPage
