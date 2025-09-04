2ef5d9de89f501ede51174af0e29e47359b235dd
Vikysan <vikysamachan@gmail.com>
2025-09-04 11:32:44 +0200

    feat: add manifest file for PWA support and update layout for manifest link

--- src/app/layout.js
+++ src/app/layout.js
@@ -19,6 +19,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <header>
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="icon" href="/icon-192x192.png" />
+        <meta name="theme-color" content="#10b981" />
+      </header>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased flex justify-center items-center`}
       >
