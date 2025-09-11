f15994f8cc7224778e89fa1d989c7f834b438a79
Vikysan <vikysamachan@gmail.com>
2025-09-11 22:03:28 +0200

    feat: refactor quiz component and remove unused page2.jsx

---
+++ src/app/test/[span]/page3).jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+// import { data } from "@/data/100";
+
+const page1 = async ({params,searchParams}) => {
+  //   const {span} = await params;
+  //   const {searchQuery} = await searchParams;
+  // console.log(params)
+  // console.log(searchParams)
+  return (
+    <div>
+  neco
+    </div>
+  )
+}
+
+export default page1
