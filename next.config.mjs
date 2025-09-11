157500aed145e8d8fc3eae105ea142ff15042aef
Vikysan <vikysamachan@gmail.com>
2025-09-11 18:02:50 +0200

    feat: update .gitignore to include public images and add allowedDevOrigins in next.config

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,7 @@ const nextConfig = {
   //   basePath: "/100",
   //   // output: "export",  // <=== enables static exports
   // reactStrictMode: true,
+  allowedDevOrigins: ['local-origin.dev', '*.local-origin.dev'],
 };
 
 export default nextConfig;
